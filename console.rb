93f9f11a3442c74d7aabbcf4bb5f91b7de3bcd17
Paul Milne <hallhill@gmail.com>
2016-10-12 14:54:55 +0100

    created save method for pets and created a pet store and one pet

--- console.rb
+++ console.rb
@@ -1,10 +1,15 @@
 require_relative('./models/pet_store')
+require_relative('./models/pets')
 
 require('pry-byebug')
 
 petcetera = PetStore.new({'name' => "Petcetera", 'address' => "Vancouver, B.C. Canada", 'type' => "house pets"})
 
 petcetera.save()
+
+pet1 = Pet.new({'name' => "Minnie", 'type' => "cat", 'pet_store_id' => petcetera.id})
+
+pet1.save()
 
 binding.pry
 nil
--- models/pets.rb
+++ models/pets.rb
@@ -11,5 +11,11 @@ class Pet
         @pet_store_id = options['pet_store_id'].to_i
     end
 
+    def save
+        sql = "INSERT INTO pets (name, type, pet_store_id) VALUES ('#{@name}', '#{@type}', '#{@pet_store_id}') RETURNING *"
+        pet = SqlRunner.run(sql).first
+        @id = pet['id']
+    end
+
 
 end
